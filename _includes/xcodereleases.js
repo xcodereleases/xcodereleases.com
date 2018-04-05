8033f44c9fd2ed6037eacca4b8b3d1f1a9e39184
xcodereleases <xcodereleases@icloud.com>
2018-04-05 00:20:03 -0600

    initial import of site code

---
+++ _includes/xcodereleases.js
@@ -0,0 +1,62 @@
+function hasContent(a) { return a.length > 0; }
+function lowercase(a) { return a.toLowerCase(); }
+
+function extractTerms(text) {
+  var terms = []
+  var myRegexp = /[^\s"]+|"([^"]*)"/gi;
+
+  do {
+    var match = myRegexp.exec(text);
+    if (match != null) {
+        terms.push(match[1] ? match[1] : match[0]);
+    }
+  } while (match != null);
+  return terms.filter(hasContent).map(lowercase)
+}
+
+function matches(node, term) {
+  if (node.nodeType === document.TEXT_NODE) {
+    var nodeText = node.nodeValue.toLowerCase();
+    if (nodeText.indexOf(term) !== -1) { return true; }
+  } else if (node.nodeType === document.ELEMENT_NODE) {
+    for (var i = 0; i < node.childNodes.length; i++) {
+      if (matches(node.childNodes[i], term)) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+function filter() {
+  var filterField = document.getElementById("filter-text");
+  var searchTerms = extractTerms(filterField.value);
+  
+  var releaseTypeButtons = document.getElementsByName("filter-release");
+  var releaseClass = null;
+  var releaseButtonCount = releaseTypeButtons.length;
+  for (var i = 0; i < releaseButtonCount; i++) {
+    if (releaseTypeButtons[i].checked == true) {
+       releaseClass = releaseTypeButtons[i].value;
+    }
+  }
+  
+  if (releaseClass.length == 0) { releaseClass = null; }
+  
+  var xcodeRows = document.getElementsByClassName("xcode");
+  var rowCount = xcodeRows.length;
+  for (var i = 0; i < rowCount; i++) {
+    var show = true;
+    if (releaseClass != null && xcodeRows[i].classList.contains(releaseClass) == false) {
+      show = false;
+    }
+    
+    if (searchTerms.length > 0 && show == true) {
+      for (var termIndex = 0; termIndex < searchTerms.length && show == true; termIndex++) {
+        show = matches(xcodeRows[i], searchTerms[termIndex]);
+      }
+    }
+    
+    xcodeRows[i].style.display = show ? "table-row" : "none";
+  }
+}
---
+++ index.md
@@ -0,0 +1,43 @@
+---
+title: Xcode Releases
+description: More than you ever wanted to know™
+---
+
+<script type="text/javascript">
+{% include xcodereleases.js %}
+</script>
+
+This is not an official Apple website. [Please consider donating](https://paypal.me/XcodeReleases) to help maintain it.
+
+<div id="search">
+  <input type="radio" name="filter-release" value="" checked  onchange="filter()" /> All
+  <input type="radio" name="filter-release" value="gm" onchange="filter()" /> GMs
+  <input type="radio" name="filter-release" value="beta" onchange="filter()" /> Betas
+  <input type="search" id="filter-text" oninput="filter()" placeholder="Search"/>
+</div>
+
+---
+
+<table id="xcodes">
+  <tr>
+    <th>Version</th>
+    <th>Release</th>
+    <th>Build</th>
+    <th>Released</th>
+    <th>Requires</th>
+    <th>macOS SDKs</th>
+    <th>iOS SDKs</th>
+    <th>watchOS SDKs</th>
+    <th>tvOS SDKs</th>
+    <th><a name="ret-fn1"></a>Download<a href="#fn1">¹</a></th>
+  </tr>
+{% for release in site.data.releases %}
+  {% include xcode_row.html release=release %}
+{% endfor %}
+</table>
+
+---
+
+<ul>
+  <li><a name="fn1"></a>¹ - If the direct download link doesn't work, you can find most downloads on <a href="https://developer.apple.com/download/more">developer.apple.com/download/more</a>.<br />Alternatively, make sure you're logged in to developer.apple.com, and then reload xcodereleases.com. <a href="#ret-fn1">[↑]</a></li>
+</ul>
