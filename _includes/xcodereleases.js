931c4d254a272a17a4252ff8dd9a539f553128c4
xcodereleases <xcodereleases@icloud.com>
2022-11-24 11:46:39 -0700

    Support "q" and "scope" query parameters
    This adds support for "q" and "scope" parameters in the URL:
    - `q`: This value is put into the search field on the page for filtering rows that match the provided text
    - `scope`: The value corresponds to the "All, Release, Betas" radio buttons
    
    Additionally, the logic of the page has changed slightly such that the URL (and not the input values) are now the source of truth: updating the input fields changes the URL, and then the URL is used to filter the rows. When the page loads, any existing URL values are placed into the input fields so that state remains consistent.
    
    Kudos to @kj800x for the idea: https://twitter.com/kevinismahname/status/1595117874653573120

--- _includes/xcodereleases.js
+++ _includes/xcodereleases.js
@@ -28,36 +28,46 @@ function matches(node, term) {
   return false;
 }
 
-function filter() {
-  var filterField = document.getElementById("filter-text");
-  var searchTerms = extractTerms(filterField.value);
+function updateRowStateToMatchFilter() {
+  const query = new URLSearchParams(document.location.search);
+  const xcodeRows = document.getElementsByClassName("xcode");
   
-  var releaseTypeButtons = document.getElementsByName("filter-release");
-  var releaseClass = null;
-  var releaseButtonCount = releaseTypeButtons.length;
-  for (var i = 0; i < releaseButtonCount; i++) {
-    if (releaseTypeButtons[i].checked == true) {
-       releaseClass = releaseTypeButtons[i].value;
+  // FIRST: show all rows (reset row state)
+  for (var r = 0; r < xcodeRows.length; r++) {
+    xcodeRows[r].style.display = "table-row";
+  }
+  
+  // SECOND: hide rows that don't match the search terms
+  const q = query.get("q");
+  if (q !== null && q.trim().length > 0) {
+    const searchTerms = extractTerms(q);
+    for (var r = 0; r < xcodeRows.length; r++) {
+      // if this row is already hidden, we don't need to check to hide it again
+	  if (xcodeRows[r].style.display === "none") { continue; }
+	  
+      var shouldHide = false;
+      for (var termIndex = 0; termIndex < searchTerms.length && shouldHide === false; termIndex++) {
+        shouldHide = !matches(xcodeRows[r], searchTerms[termIndex]);
+      }
+      
+      if (shouldHide === true) { xcodeRows[r].style.display = "none"; }
     }
   }
   
-  if (releaseClass.length == 0) { releaseClass = null; }
-  
-  var xcodeRows = document.getElementsByClassName("xcode");
-  var rowCount = xcodeRows.length;
-  for (var i = 0; i < rowCount; i++) {
-    var show = true;
-    if (releaseClass != null && xcodeRows[i].classList.contains(releaseClass) == false) {
-      show = false;
-    }
-    
-    if (searchTerms.length > 0 && show == true) {
-      for (var termIndex = 0; termIndex < searchTerms.length && show == true; termIndex++) {
-        show = matches(xcodeRows[i], searchTerms[termIndex]);
+  // THIRD: hide rows that don't have the proper release type  
+  const scope = query.get("scope");
+  if (scope !== null) {
+    for (var r = 0; r < xcodeRows.length; r++) {
+      // if this row is already hidden, we don't need to check to hide it again
+	  if (xcodeRows[r].style.display === "none") { continue; }
+	  
+      var shouldHide = false;
+      if (scope != null && xcodeRows[r].classList.contains(scope) == false) {
+        shouldHide = true;
       }
+      
+      if (shouldHide === true) { xcodeRows[r].style.display = "none"; }
     }
-    
-    xcodeRows[i].style.display = show ? "table-row" : "none";
   }
 }
 
@@ -73,3 +83,70 @@ function toggleDetails(rowID) {
     details.style.display = "block";
   }
 }
+
+function filter() {
+  // Update the URL in the location bar with the new state
+  // this updates the URL in the location bar without reloading the page, and without updating browser history.
+  // Otherwise, typing into the search field would generate a history entry for each keystroke.
+  var url = new URL(document.location);
+  
+  // update the q parameter
+  const filterField = document.getElementById("filter-text");
+  const queryQ = filterField.value.trim();
+  if (queryQ === null || queryQ.trim().length == 0) {
+    url.searchParams.delete("q");
+  } else {
+    url.searchParams.set("q", queryQ);
+  }
+  
+  // update the scope parameter
+  const releaseTypeButtons = document.getElementsByName("filter-release");
+  var queryScope = null;
+  for (var i = 0; i < releaseTypeButtons.length; i++) {
+    if (releaseTypeButtons[i].checked == true) {
+       queryScope = releaseTypeButtons[i].value;
+    }
+  }
+  if (queryScope === null || queryScope.trim().length == 0) {
+    url.searchParams.delete("scope");
+  } else {
+    url.searchParams.set("scope", queryScope);
+  }
+  
+  window.history.replaceState({}, document.title, url);
+  
+  updateRowStateToMatchFilter();
+}
+
+function prefillFilter() {
+  const params = new URLSearchParams(window.location.search);
+  
+  var filterCount = 0;
+  
+  // set the "q" parameter into the search field
+  const q = params.get("q");
+  if (q !== null && q.trim().length > 0) {
+    const filterField = document.getElementById("filter-text");
+    filterField.value = q;
+    filterCount += 1;
+  }
+  
+  // set the "scope" parameter into the radio buttons
+  const scope = params.get("scope");
+  if (scope !== null && scope.trim().length > 0) {
+    var releaseTypeButtons = document.getElementsByName("filter-release");
+    var releaseButtonCount = releaseTypeButtons.length;
+    for (var i = 0; i < releaseTypeButtons.length; i++) {
+      if (releaseTypeButtons[i].value === scope) {
+        releaseTypeButtons[i].checked = true
+        filterCount += 1;
+      }
+    }
+  }
+  
+  updateRowStateToMatchFilter();
+}
+
+window.addEventListener("load", function(){
+  prefillFilter();
+});
